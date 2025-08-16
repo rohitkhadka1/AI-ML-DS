b90addbdf587c74cb5a3852494a9f2f8b8c75201
Rohit Khadka <rohitkhadka153@gmail.com>
2025-08-16 23:12:37 +0530

    Tried openai embedding models

---
+++ text_embedding_3_large_embeddings.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os 
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENROUTER_API_KEY")
+print(OPENAI_API_KEY)
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=OPENAI_API_KEY,
+)
+
+response = client.embeddings.create(
+    model="text-embedding-3-large",
+    input="What is the capital city of Nepal ?"
+    )
+print(response.data[0].embedding)
